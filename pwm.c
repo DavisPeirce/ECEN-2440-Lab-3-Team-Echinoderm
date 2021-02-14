4f7474736c5684905778f9688f7cea2e1701a766
Terence646 <77753599+Terence646@users.noreply.github.com>
2021-02-14 16:39:41 -0700

    All finished functions except button_config

--- pwm.c
+++ pwm.c
@@ -7,18 +7,20 @@
  */
 
 void timer_pwmconfig(void){
-    TIMER_A0->CTL &= TIMER_A_CTL_MC__STOP;
-    TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK;
-    TIMER_A0->CTL |= TIMER_A_CTL_ID__1;
-
-    TIMER_A0 ->CCTL |= TIMER_A_CCTLN_OUTMOD_4;
+    TIMER_A0->CTL       |= TIMER_A_CTL_CLR;
+    TIMER_A0->CTL       |= TIMER_A_CTL_SSEL__SMCLK;
+    TIMER_A0->CTL       |= TIMER_A_CTL_ID__1;
+    TIMER_AO->CCR[0]    |= TICKS;
+    TIMER_A0->CCTL      |= TIMER_A_CCTLN_OUTMOD_4;
 
     //TIMER_A0->CTL |= TIMER_A_CTL_IE;
 }
 
 void pwm_gpio_config(void){
-    P2->DIR |= BIT4;
-
+    P2->DIR     |= BIT4;
+    P2->OUT     |= BIT4;                    // Make sure that P2 is an output
+    P2->SEL0    |= BIT4;                    // Select the mode
+    P2->SEL1    &= ~(BIT4);
 }
 
 void start_pwm(void){
@@ -28,6 +30,10 @@ void start_pwm(void){
 
 void stop_pwm(void){
     TIMER_A0->CTL &= ~(TIMER_A_CTL_MC__STOP);
+}
+
+void button_config(void){
+
 }
 
 
--- pwm.h
+++ pwm.h
@@ -9,28 +9,20 @@
 #ifndef PWM_H_
 #define PWM_H_
 
+/* DEFINES */
+/* TICKS = (f_src/(2^ID))*(1/(0.5*f_desired)
+* Example: TICKS = ((3MHz)/(2^4)*(1/30Hz)) = 6250 in decimal
+*/
+#define TICKS ((uint16_t)0x001E)
 
 void timer_pwmconfig(void);
 
 void pwm_gpio_config(void);
 
 /* Start the Timer */
-void star_pwm(void);
-
-/* Stop the Timer */
-void stop_pwm(void);
-
-
-#endif /* PWM_H_ */
-/* Configure TIMER_A0 to produce PWM waveform
-void config_pwm_timer(void);
-/* Start the Timer */
 void start_pwm(void);
 
-
-/* Config P2.4 to output TA0.1 waveform */
-void config_pwm_gpio(void);
-
-
 /* Stop the Timer */
 void stop_pwm(void);
+
+void button_config(void);
